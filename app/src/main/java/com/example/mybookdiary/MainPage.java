f6f9f3f47b6eaec81fdde2d4d48963da436bfb51
OljaM3005 <olja3005@gmail.com>
2023-01-23 01:03:20 +0100

    PH fragments navigation finished

--- app/src/main/java/com/example/mybookdiary/MainPage.java
+++ app/src/main/java/com/example/mybookdiary/MainPage.java
@@ -1,14 +1,49 @@
 package com.example.mybookdiary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainPage extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainPage extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+    DiaryFragment diaryFragment =  new DiaryFragment();
+    FindBookFragment findBookFragment = new FindBookFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, diaryFragment).commit();
+                return true;
+            case R.id.search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, findBookFragment).commit();
+                return true;
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, profileFragment).commit();
+                return  true;
+
+
+        }
+
+        return false;
     }
 }
