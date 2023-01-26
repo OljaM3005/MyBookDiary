3e680fd7427c20b99fd44eacf21e56e3ab5c4b96
OljaM3005 <olja3005@gmail.com>
2023-01-26 05:24:58 +0100

    - changed constraints in .xml activity files - added toolbar in fragment_diary.xml

---
+++ app/src/main/java/com/example/mybookdiary/db/DatabaseHelper.java
@@ -0,0 +1,4 @@
+package com.example.mybookdiary.db;
+
+public class DatabaseHelper {
+}
---
+++ app/src/main/java/com/example/mybookdiary/db/entity/Books.java
@@ -0,0 +1,14 @@
+package com.example.mybookdiary.db.entity;
+
+public class Books {
+
+    // 1- Constants for Database
+
+
+
+
+
+
+
+
+}
